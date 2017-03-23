b924b3b1733d3ce9c86c4e0a0719e2209b232540
tejvuligonda <tejvuligonda@gmail.com>
2017-03-23 12:33:05 -0700

    Updated submit.php to only change the interval_ms in the JSON. Issue #13

--- submit.php
+++ submit.php
@@ -8,25 +8,32 @@
 <body>
 <?php
 
-$time = $_POST["time"];
+if ($_POST) {
+    $time = $_POST["time"];
 
-$stop = $_POST["stop"];
+    $stop = $_POST["stop"];
 
-$myfile = fopen("config.json", "w");
-if ($stop) {
-    $json = "{ \"interval_ms\": 0 }"; 
-    echo '<p>The slideshow is now stopped. To start it again, please input an interval time.</p>';
+    $jsonString = file_get_contents('config.json');
+    $jsonArray = json_decode($jsonString, true);
+    
+    if ($stop) {
+        $jsonArray['interval_ms'] = 0;
+        echo '<p>The slideshow is now stopped. To start it again, please input an interval time.</p>';
+    }
+    else {
+        $jsonArray['interval_ms'] = $time;
+        echo "<p>The interval between slides is now set to " . $time . " ms</p>";
+    }
+    $newJsonString = json_encode($jsonArray);
+    print_r($jsonArray);
+    file_put_contents('config.json', $newJsonString); 
 }
+
 else {
-    $json = "{ \"interval_ms\": $time }";
-    echo "<p>The interval between slides is now set to " . $time . " ms</p>";
+    echo 'Please update the slideshow <a href="update.html">here</a> if you wish to use this page.</br>';
 }
+
 echo 'You are being returned to the slideshow now. Click <a href="carousel.php">here</a> if you are not redirected.';
-fwrite($myfile, $json);
-fclose($myfile);
-
-
-
 ?>
 </body>
 </html>
